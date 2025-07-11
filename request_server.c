d31cc66abeff509249d522ff9c8a3d0b72a301d7
Edoardo Tinto <tinto.edoardo@gmail.com>
2025-07-11 11:56:51 +0200

    Requests server completed

--- request_server.c
+++ request_server.c
@@ -24,11 +24,26 @@ struct State {
     // not 0  -> true
     int should_migrate;
     sem_t *semaphore;
+    const char *path_to_main_memory;
+    const char *path_to_checkpoint_memory;
 };
 
 ///  Support function for logging error.
 static void exit_with_error(const char *message, wasmtime_error_t *error,
-                            wasm_trap_t *trap);
+                            wasm_trap_t *trap) {
+    fprintf(stderr, "error: %s\n", message);
+    wasm_byte_vec_t error_message;
+    if (error != NULL) {
+        wasmtime_error_message(error, &error_message);
+        wasmtime_error_delete(error);
+    } else {
+        wasm_trap_message(trap, &error_message);
+        wasm_trap_delete(trap);
+    }
+    fprintf(stderr, "%.*s\n", (int)error_message.size, error_message.data);
+    wasm_byte_vec_delete(&error_message);
+    exit(1);
+};
 
 ///  Should_migrate callback, this function is
 ///  imported in the Wasm module.
@@ -64,14 +79,55 @@ static wasm_trap_t *should_migrate(void *env, wasmtime_caller_t *caller,
 static wasm_trap_t *restore_memory(void *env, wasmtime_caller_t *caller,
                                       const wasmtime_val_t *args, size_t nargs,
                                       wasmtime_val_t *results, size_t nresults) {
+    // Extract the caller state.
+    wasmtime_context_t *context = wasmtime_caller_context(caller);
+    struct State *state = wasmtime_context_get_data(context);
 
+    const char *path_to_main_memory       = state->path_to_main_memory;
+    const char *path_to_checkpoint_memory = state->path_to_checkpoint_memory;
+
+    wasmtime_memory_t memory, checkpoint_memory;
+    wasmtime_extern_t memory_item, checkpoint_memory_item;
+
+    // Get the main linear memory from the caller.
+    wasmtime_caller_export_get(caller, "memory", strlen("memory"), &memory_item);
+    memory = memory_item.of.memory;
+    uint8_t *memory_ref = wasmtime_memory_data(context, &memory);
+
+    // Get the checkpoint memory from the caller.
+    wasmtime_caller_export_get(caller, "checkpoint_memory", strlen("checkpoint_memory"), &checkpoint_memory_item);
+    checkpoint_memory = checkpoint_memory_item.of.memory;
+    uint8_t *checkpoint_memory_ref = wasmtime_memory_data(context, &checkpoint_memory);
+
+    //----------------------------------//
+    //      Memory Initialization       //
+    //----------------------------------//
+    // This step is performed at runtime.
+    FILE *main_memory_fd = fopen(path_to_main_memory, "r");
+    if(main_memory_fd != NULL) {
+        fread(memory_ref, 1, 64 * 1024, main_memory_fd);
+    } // Ignore otherwise.
+    fclose(main_memory_fd);
+
+    FILE *checkpoint_memory_fd = fopen(path_to_checkpoint_memory, "r");
+    if(checkpoint_memory_fd != NULL) {
+        // Note that we are reading at most 4KB from the checkpoint_memory.
+        fread(checkpoint_memory_fd, 1, 4 * 1024, main_memory_fd);
+    } // Ignore otherwise.
+    fclose(checkpoint_memory_fd);
+
+    return NULL;
 }
 
 ///  The workload of a request server, it initialize the computation
 ///  then sleep waiting for the activation command. While running
 ///  a Wasm function, if a migration request is issued, it suspend the
 ///  execution and produce a checkpoint.
-int request_server_workload(const char *path_to_file, const char *path_to_ipc_file) {
+int request_server_workload(
+        const char *path_to_file,
+        const char *path_to_ipc_file,
+        const char *path_to_main_memory,
+        const char *path_to_checkpoint_memory) {
 
     //----------------------------------//
     //    Inter-Process Communication   //
@@ -110,12 +166,17 @@ int request_server_workload(const char *path_to_file, const char *path_to_ipc_fi
 
     // The state of the Wasm computation.
     struct State state;
-    state.should_migrate = 0;
-    state.semaphore      = should_migrate_sem;
+    state.should_migrate            = 0;
+    state.semaphore                 = should_migrate_sem;
+    state.path_to_main_memory       = path_to_main_memory;
+    state.path_to_checkpoint_memory = path_to_checkpoint_memory;
 
     //  Initialize the engine.
     printf("Initializing...\n");
-    wasm_engine_t *engine = wasm_engine_new();
+    wasm_config_t *config = wasm_config_new();
+    assert(config != NULL);
+    wasmtime_config_wasm_multi_memory_set(config, true);
+    wasm_engine_t *engine = wasm_engine_new_with_config(config);
     assert(engine != NULL);
 
     // Initialize the store.
@@ -146,7 +207,7 @@ int request_server_workload(const char *path_to_file, const char *path_to_ipc_fi
         exit_with_error("failed to parse wat", error, NULL);
     wasm_byte_vec_delete(&wat);
 
-    // Now that we've got our binary webassembly we can compile our module.
+    // Compile the module.
     printf("Compiling module...\n");
     wasmtime_module_t *module = NULL;
     error = wasmtime_module_new(engine, (uint8_t *)wasm.data, wasm.size, &module);
@@ -160,140 +221,54 @@ int request_server_workload(const char *path_to_file, const char *path_to_ipc_fi
     wasmtime_func_t should_migrate_t;
     wasmtime_func_new(context, should_migrate_ty, should_migrate, NULL, NULL, &should_migrate_t);
 
-    // With our callback function we can now instantiate the compiled module,
-    // giving us an instance we can then execute exports from. Note that
-    // instantiation can trap due to execution of the `start` function, so we need
-    // to handle that here too.
+    wasm_functype_t *restore_memory_ty = wasm_functype_new_0_0();
+    wasmtime_func_t restore_memory_t;
+    wasmtime_func_new(context, restore_memory_ty, restore_memory, NULL, NULL, &restore_memory_t);
+
+    // Instantiate the module.
     printf("Instantiating module...\n");
     wasm_trap_t *trap = NULL;
     wasmtime_instance_t instance;
-    wasmtime_extern_t import;
-    import.kind = WASMTIME_EXTERN_FUNC;
-    import.of.func = should_migrate_t;
-    error = wasmtime_instance_new(context, module, &import, 1, &instance, &trap);
+    wasmtime_extern_t imports[2];
+    imports[0].kind = WASMTIME_EXTERN_FUNC;
+    imports[0].of.func = should_migrate_t;
+    imports[1].kind = WASMTIME_EXTERN_FUNC;
+    imports[1].of.func = restore_memory_t;
+    error = wasmtime_instance_new(context, module, imports, 1, &instance, &trap);
     if (error != NULL || trap != NULL)
         exit_with_error("failed to instantiate", error, trap);
 
-    // Lookup our `run` export function
+    // Lookup our `run` export function.
     printf("Extracting export...\n");
     wasmtime_extern_t run;
     bool ok = wasmtime_instance_export_get(context, &instance, "run", 3, &run);
     assert(ok);
     assert(run.kind == WASMTIME_EXTERN_FUNC);
 
-}
+    //----------------------------------//
+    //       Suspend until START        //
+    //----------------------------------//
 
-static wasm_trap_t *hello_callback(void *env, wasmtime_caller_t *caller,
-                                   const wasmtime_val_t *args, size_t nargs,
-                                   wasmtime_val_t *results, size_t nresults) {
-    printf("Calling back...\n");
-    printf("> Hello World!\n");
-    return NULL;
-}
+    sem_wait(proceed_to_run);
+    // Once the semaphore is relinquished, proceed to:
 
-int thing() {
-    int ret = 0;
-    // Set up our compilation context. Note that we could also work with a
-    // `wasm_config_t` here to configure what feature are enabled and various
-    // compilation settings.
-    printf("Initializing...\n");
-    wasm_engine_t *engine = wasm_engine_new();
-    assert(engine != NULL);
+    //----------------------------------//
+    //         Run the function         //
+    //----------------------------------//
 
-    // With an engine we can create a *store* which is a long-lived group of wasm
-    // modules. Note that we allocate some custom data here to live in the store,
-    // but here we skip that and specify NULL.
-    wasmtime_store_t *store = wasmtime_store_new(engine, NULL, NULL);
-    assert(store != NULL);
-    wasmtime_context_t *context = wasmtime_store_context(store);
-
-    // Read our input file, which in this case is a wasm text file.
-    FILE *file = fopen("examples/hello.wat", "r");
-    assert(file != NULL);
-    fseek(file, 0L, SEEK_END);
-    size_t file_size = ftell(file);
-    fseek(file, 0L, SEEK_SET);
-    wasm_byte_vec_t wat;
-    wasm_byte_vec_new_uninitialized(&wat, file_size);
-    if (fread(wat.data, file_size, 1, file) != 1) {
-        printf("> Error loading module!\n");
-        return 1;
-    }
-    fclose(file);
-
-    // Parse the wat into the binary wasm format
-    wasm_byte_vec_t wasm;
-    wasmtime_error_t *error = wasmtime_wat2wasm(wat.data, wat.size, &wasm);
-    if (error != NULL)
-        exit_with_error("failed to parse wat", error, NULL);
-    wasm_byte_vec_delete(&wat);
-
-    // Now that we've got our binary webassembly we can compile our module.
-    printf("Compiling module...\n");
-    wasmtime_module_t *module = NULL;
-    error = wasmtime_module_new(engine, (uint8_t *)wasm.data, wasm.size, &module);
-    wasm_byte_vec_delete(&wasm);
-    if (error != NULL)
-        exit_with_error("failed to compile module", error, NULL);
-
-    // Next up we need to create the function that the wasm module imports. Here
-    // we'll be hooking up a thunk function to the `hello_callback` native
-    // function above. Note that we can assign custom data, but we just use NULL
-    // for now).
-    printf("Creating callback...\n");
-    wasm_functype_t *hello_ty = wasm_functype_new_0_0();
-    wasmtime_func_t hello;
-    wasmtime_func_new(context, hello_ty, hello_callback, NULL, NULL, &hello);
-
-    // With our callback function we can now instantiate the compiled module,
-    // giving us an instance we can then execute exports from. Note that
-    // instantiation can trap due to execution of the `start` function, so we need
-    // to handle that here too.
-    printf("Instantiating module...\n");
-    wasm_trap_t *trap = NULL;
-    wasmtime_instance_t instance;
-    wasmtime_extern_t import;
-    import.kind = WASMTIME_EXTERN_FUNC;
-    import.of.func = hello;
-    error = wasmtime_instance_new(context, module, &import, 1, &instance, &trap);
-    if (error != NULL || trap != NULL)
-        exit_with_error("failed to instantiate", error, trap);
-
-    // Lookup our `run` export function
-    printf("Extracting export...\n");
-    wasmtime_extern_t run;
-    bool ok = wasmtime_instance_export_get(context, &instance, "run", 3, &run);
-    assert(ok);
-    assert(run.kind == WASMTIME_EXTERN_FUNC);
-
-    // And call it!
+    // Call the function.
     printf("Calling export...\n");
     error = wasmtime_func_call(context, &run.of.func, NULL, 0, NULL, 0, &trap);
     if (error != NULL || trap != NULL)
         exit_with_error("failed to call function", error, trap);
 
-    // Clean up after ourselves at this point
+    // Finalize.
     printf("All finished!\n");
-    ret = 0;
-
     wasmtime_module_delete(module);
     wasmtime_store_delete(store);
     wasm_engine_delete(engine);
-    return ret;
-}
 
-static void exit_with_error(const char *message, wasmtime_error_t *error,
-                            wasm_trap_t *trap) {
-    fprintf(stderr, "error: %s\n", message);
-    wasm_byte_vec_t error_message;
-    if (error != NULL) {
-        wasmtime_error_message(error, &error_message);
-        wasmtime_error_delete(error);
-    } else {
-        wasm_trap_message(trap, &error_message);
-        wasm_trap_delete(trap);
-    }
-    fprintf(stderr, "%.*s\n", (int)error_message.size, error_message.data);
-    wasm_byte_vec_delete(&error_message);
-    exit(1);
+    // Then return.
+    return 0;
+
 }
